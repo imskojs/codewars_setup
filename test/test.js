b162b27f0a24e5b243dc72d0335992604dbb58aa
Seunghoon Ko <imskojs@gmail.com>
2016-09-12 07:11:25 +0900

    Find The Parity Outlier test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 let assert = require('chai').assert;
 require('../src/jaden_casing_strings');
 let calculateYears = require('../src/money,money,money');
+let findOutlier = require('../src/find_the_parity_outlier');
 
 describe('Jaden Casing String', () => {
   it('should capitalize every word', () => {
@@ -18,3 +19,12 @@ describe('Money,Money,Money', () => {
   });
 });
 
+describe('what?', function () {
+  it('should return N, unique odd or even number(outlier)', function () {
+    assert.equal(findOutlier([0, 1, 2]), 1);
+    assert.equal(findOutlier([1, 2, 3]), 2);
+    assert.equal(findOutlier([2,6,8,10,3]), 3);
+    assert.equal(findOutlier([0,0,3,0,0]), 3);
+    assert.equal(findOutlier([1,1,0,1,1]), 0);
+  });
+});

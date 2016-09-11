ce54e543628829537cd19c79a865c8dd873b5909
Seunghoon Ko <imskojs@gmail.com>
2016-09-12 06:31:57 +0900

    test environment

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 let assert = require('chai').assert;
-require('./jaden_casing_strings');
-let calculateYears = require('./money,money,money');
+require('../src/jaden_casing_strings');
+let calculateYears = require('../src/money,money,money');
 
 describe('Jaden Casing String', () => {
   it('should capitalize every word', () => {
@@ -11,7 +11,7 @@ describe('Jaden Casing String', () => {
 });
 
 describe('Money,Money,Money', () => {
-  it('should return correct number of years to achieve desired', () => {
+  it('should return correct number of years to achieve desired amount', () => {
     assert.equal(calculateYears(1000, 0.05, 0.18, 1100), 3);
     assert.equal(calculateYears(1000,0.01625,0.18,1200), 14);
     assert.equal(calculateYears(1000,0.05,0.18,1000), 0);
